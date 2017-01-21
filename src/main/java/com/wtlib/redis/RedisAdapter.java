0ca4603fa20c7b0b70b1037dffd37a3074b49161
NoNException <545670755@qq.com>
2017-01-21 14:18:22 +0800

    去掉乱码

--- src/main/java/com/wtlib/redis/RedisAdapter.java
+++ src/main/java/com/wtlib/redis/RedisAdapter.java
@@ -1,28 +1,12 @@
-                    package com.wtlib.redis;
+package com.wtlib.redis;
 
-/**
- * ClassName: RedisAdapter
- * 
- * @Description: 缁熶竴绠＄悊redis鍛藉悕绌洪棿,闃叉瀛樺偍涓嶅悓瀵硅薄鏃秌ey閲嶅
- * @author dapengniao
- * @date 2016骞�鏈�鏃�涓嬪崍2:57:12
- */
 public enum RedisAdapter {
-	IP_LOCATION("a", "CHUAN"), LATEST_MESSAGE("b",
-			"lis"), USER("c", "USER_serId");
+	IP_LOCATION("a", "CHUAN"), LATEST_MESSAGE("b", "lis"), USER("c",
+			"USER_serId");
 
-	// 缂撳瓨鍚嶇О
 	private String cacheName;
-	// 鍛藉悕绌洪棿锛屼綔涓簁ey鐨勫墠缂�敤浜庡尯鍒嗕笉鍚屽瓨鍌ㄧ被鍨嬩箣闂寸殑key
 	private String nameSpace;
 
-	/**
-	 * @Description: TODO
-	 * @param @param nameSpace 鍛藉悕绌洪棿锛屼綔涓簁ey鐨勫墠缂�敤浜庡尯鍒嗕笉鍚屽瓨鍌ㄧ被鍨嬩箣闂寸殑key
-	 * @param @param cacheName 缂撳瓨鍚嶇О
-	 * @author dapengniao
-	 * @date 2016骞�鏈�鏃�涓嬪崍2:57:29
-	 */
 	private RedisAdapter(String nameSpace, String cacheName) {
 		this.nameSpace = nameSpace;
 	}
