66126b2691b209b67291294b790c86ada88143dc
ed <ed@192.168.4.101>
2017-02-16 22:45:36 +0800

    dddd

--- src/test/java/com/test/MainTest.java
+++ src/test/java/com/test/MainTest.java
@@ -10,7 +10,7 @@ import com.wtlib.mq.MQDemo;
 import com.wtlib.pojo.User;
 import com.wtlib.service.UserService;
 import com.wtlib.start.InterfaceUrlInti;
-
+//dddddddddddddddddddddddddddddddd
 
 //@RunWith(SpringJUnit4ClassRunner.class)
 //@ContextConfiguration(locations = { "classpath:spring.xml",
