1c7a4d5b006f414024d6fcc139451a768d7efeb3
NoNException <545670755@qq.com>
2017-02-16 23:14:41 +0800

    modifyed mainTest.java

--- src/test/java/com/test/MainTest.java
+++ src/test/java/com/test/MainTest.java
@@ -10,7 +10,7 @@ import com.wtlib.mq.MQDemo;
 import com.wtlib.pojo.User;
 import com.wtlib.service.UserService;
 import com.wtlib.start.InterfaceUrlInti;
-//dddddsssssssssssssss
+//ddsssssss
 
 //@RunWith(SpringJUnit4ClassRunner.class)
 //@ContextConfiguration(locations = { "classpath:spring.xml",
