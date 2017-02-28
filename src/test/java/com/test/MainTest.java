061b0a32d41fa7fbb34158397ee5095fc4030aa0
NoNException <545670755@qq.com>
2017-02-28 10:08:39 +0800

    changes

--- src/test/java/com/test/MainTest.java
+++ src/test/java/com/test/MainTest.java
@@ -1,4 +1,5 @@
 package com.test;
+
 //flicts test
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -12,36 +13,35 @@ import com.wtlib.service.UserService;
 import com.wtlib.start.InterfaceUrlInti;
 //ddsssssss
 
-
 //@RunWith(SpringJUnit4ClassRunner.class)
 //@ContextConfiguration(locations = { "classpath:spring.xml",ddddddddddddddddddddddddd
 //		"classpath:spring-mybatis.xml", "classpath:spring-mvc.xml"})
-public class MainTest{
-	 
+public class MainTest {
+
 	private static ClassPathXmlApplicationContext context = null;
-	
-	
-	static{
+
+	static {
 		InterfaceUrlInti.init();
 		context = new ClassPathXmlApplicationContext(new String[] {
 				"classpath:spring-mybatis.xml", "classpath:spring.xml",
 				"classpath:spring-aop.xml" });
 	}
-	
+
 	private static UserService userService;
+
 	@Test
-	public void test1() throws Exception {	
-		userService = (UserService)context.getBean("userService");
+	public void test1() throws Exception {
+		userService = (UserService) context.getBean("userService");
 		User user = new User();
 		user.setPassword("aaaaa");
 		user.setLoginId("testtesttest");
-		try{
+		try {
 			int insert = userService.insert(user);
 			System.out.println(insert);
-			
-		}catch(Exception e){
+
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
-	 }
-	
+	}
+
 }
